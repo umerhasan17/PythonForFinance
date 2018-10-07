10caaedcf83ca048def2b046fc9d58c57a3cb536
umerhasan17 <umerhasan17@gmail.com>
2018-10-07 12:59:37 +0100

    Creating 100 day moving average

--- file.py
+++ file.py
@@ -6,15 +6,10 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-# start = dt.datetime(2000, 1, 1)
-# end = dt.datetime(2016, 12, 31)
-
-# df = web.DataReader('TSLA', 'yahoo', start, end)
-
-# df.to_csv('tsla.csv')
-
 df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
 
-df.plot()
-plt.show()
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+df.dropna(inplace=True)
+
+print(df.tail())
 
